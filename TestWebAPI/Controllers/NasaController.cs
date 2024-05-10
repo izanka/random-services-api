d3c1e4d136c804e17c73678ecb842728222f45ec
izanka <kaliya.iza@gmail.com>
2024-05-11 00:23:57 +0530

    NASA Controller changes to get response

--- TestWebAPI/Controllers/NasaController.cs
+++ TestWebAPI/Controllers/NasaController.cs
@@ -6,14 +6,27 @@ namespace RandomServicesAPI.Controllers
     [ApiController]
     public class NasaController : ControllerBase
     {
-        public IActionResult GetImageOfTheDay()
+        HttpClient _httpClient = new HttpClient();
+
+        public NasaController(HttpClient httpClient) {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet("getImageOfTheDay")]
+        public async Task<IActionResult> GetImageOfTheDay()
         {
-            var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=DEMO_KEY";
-            HttpClient client = new HttpClient();
+            var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=DEMO_KEY"; 
 
-            var response = client.GetAsync(url);
+            var response = await _httpClient.GetAsync(url);
 
-            return Ok();
+            if (response.IsSuccessStatusCode)
+            {
+                var content = response.Content.ReadAsStringAsync();
+                return Ok(content);
+            }
+            else {
+                return StatusCode((int)response.StatusCode);
+            }
         }
     }
 }
--- TestWebAPI/obj/Debug/net8.0/RandomServicesAPI.AssemblyInfo.cs
+++ TestWebAPI/obj/Debug/net8.0/RandomServicesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RandomServicesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a252d8c9898e7c6051e646d125841954674b0677")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5beb657a8af491f54cee47af77aa8ed78f9af27")]
 [assembly: System.Reflection.AssemblyProductAttribute("RandomServicesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RandomServicesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
