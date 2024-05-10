d5beb657a8af491f54cee47af77aa8ed78f9af27
izanka <kaliya.iza@gmail.com>
2024-05-10 23:57:28 +0530

    Added NASA controller

---
+++ TestWebAPI/Controllers/NasaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RandomServicesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NasaController : ControllerBase
+    {
+        public IActionResult GetImageOfTheDay()
+        {
+            var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=DEMO_KEY";
+            HttpClient client = new HttpClient();
+
+            var response = client.GetAsync(url);
+
+            return Ok();
+        }
+    }
+}
--- TestWebAPI/obj/Debug/net8.0/RandomServicesAPI.AssemblyInfo.cs
+++ TestWebAPI/obj/Debug/net8.0/RandomServicesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RandomServicesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+931fab8e9c281c6a875b8b9c5ab23e4af7be54ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a252d8c9898e7c6051e646d125841954674b0677")]
 [assembly: System.Reflection.AssemblyProductAttribute("RandomServicesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RandomServicesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
